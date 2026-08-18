a768df773e99fd9a1111334448b0a872b2760197
Matsvei Khatkevich <matsvei.khatkevich@innowise.com>
2026-08-18 15:20:43 +0200

    init commit

---
+++ src/main/java/com/innowise/telemetry_ingestion_service/TelemetryIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.telemetry_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryIngestionServiceApplication.class, args);
+	}
+
+}
