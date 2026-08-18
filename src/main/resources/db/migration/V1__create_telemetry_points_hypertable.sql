80d3b8fdf3d60f4108bdc67868424ee7807e112a
Danil0ss <2005prudnikov@gmail.com>
2026-08-19 01:39:16 +0300

    feat(TIRI-3): add Flyway migration for telemetry_points hypertable

---
+++ src/main/resources/db/migration/V1__create_telemetry_points_hypertable.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE TABLE telemetry_points (
+    time TIMESTAMPTZ NOT NULL,
+    device_id UUID NOT NULL,
+    location GEOMETRY(Point, 4326) NOT NULL,
+    speed REAL,
+    altitude REAL,
+    sensors JSONB,
+    PRIMARY KEY (device_id,time)
+);
+
+SELECT create_hypertable('telemetry_points', 'time', chunk_time_interval => INTERVAL '1 day');
+
+ALTER TABLE telemetry_points SET (
+    timescaledb.compress,
+    timescaledb.compress_segmentby = 'device_id'
+    );
+SELECT add_compression_policy('telemetry_points', INTERVAL '7 days');
+
