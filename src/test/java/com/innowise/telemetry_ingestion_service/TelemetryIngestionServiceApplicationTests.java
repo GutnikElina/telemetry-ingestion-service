bc699103f7a8df1fd9ba50d64174cc9a6be850ca
yaaanni <rw.yana.rw@gmail.com>
2026-08-19 19:50:34 +0300

    ci: ad ci pipeline

--- src/test/java/com/innowise/telemetry_ingestion_service/TelemetryIngestionServiceApplicationTests.java
+++ src/test/java/com/innowise/telemetry_ingestion_service/TelemetryIngestionServiceApplicationTests.java
@@ -2,14 +2,11 @@ package com.innowise.telemetry_ingestion_service;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+// @SpringBootTest
 class TelemetryIngestionServiceApplicationTests {
 
-    @Test
-    @Disabled("Временно отключен для проверки CI")
-    void contextLoads() {
-    }
-
+  @Test
+  @Disabled("Временно отключен для проверки CI")
+  void contextLoads() {}
 }
