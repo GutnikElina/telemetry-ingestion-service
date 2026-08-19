4213cc8c2345730a9689ec3b96e6a84d856011ce
yaaanni <rw.yana.rw@gmail.com>
2026-08-19 18:36:12 +0300

    ci: add ci pipeline

---
+++ src/test/java/com/innowise/telemetry_ingestion_service/PipelineTest.java
@@ -0,0 +1,12 @@
+package com.innowise.telemetry_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PipelineTest {
+    @Test
+    void dummyTestForPipeline() {
+        assertEquals(2, 1 + 1);
+    }
+}
